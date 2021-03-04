edf15425e373a38a06353d598ff9b95924ef24b7
omar-perez-cano-itlighten <is718089@iteso.mx>
2021-03-03 19:26:37 -0600

    Initial commit

---
+++ lib/bloc/front_bloc.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'front_event.dart';
+part 'front_state.dart';
+
+class FrontBloc extends Bloc<FrontEvent, FrontState> {
+  FrontBloc() : super(FrontInitial());
+
+  List<String> list1 = [
+    "Perro",
+    "Gato",
+    "Mono",
+    "Simio",
+    "Camilla",
+    "Kamikaze"
+  ];
+  List<String> list2 = [];
+
+  int listSize = 0;
+  int index = 0;
+  int count = 0;
+
+  @override
+  Stream<FrontState> mapEventToState(
+    FrontEvent event,
+  ) async* {
+    if (event is StartEvent) {
+      listSize = list1.length;
+      list2 = list1..shuffle();
+      count = 0;
+      index = 0;
+
+      yield JuegoIniciadoState(
+          contador: count, palabra: list2[index], titulo: "The word is...");
+    } else if (event is SkipEvent) {
+      if (list1[index] != list2[index]) count++;
+      index++;
+
+      if (index < listSize)
+        yield JuegoIniciadoState(
+            contador: count, palabra: list2[index], titulo: "The word is....");
+      else
+        yield JuegoEndState(contador: count, titulo: "You Scored");
+    } else if (event is GotEvent) {
+      if (list1[index] == list2[index]) count++;
+      index++;
+
+      if (index < listSize)
+        yield JuegoIniciadoState(
+            contador: count, palabra: list2[index], titulo: "The word is....");
+      else
+        yield JuegoEndState(contador: count, titulo: "You Scored");
+    } else if (event is EndEvent) {
+      yield JuegoEndState(contador: count, titulo: "You Scored");
+    }
+  }
+}
